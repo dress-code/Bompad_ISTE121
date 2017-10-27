84525908cb447ae5c7a4db4699c5550203b2e8a3
Compulex <33073741+Compulex@users.noreply.github.com>
2017-10-26 20:12:59 -0400

    Bompad class

--- Bompad.java
+++ Bompad.java
@@ -1,6 +1,51 @@
 
 
-public class Bompad{
-
-
-}
+public class Bompad extends JFrame{
+   
+   public static void main(String [] args)
+   {
+      new Bompad();
+   }//end main
+   
+   public Bompad()
+   {
+      //MENUS
+      JMenuBar jmb = new JMenuBar();
+      setJMenuBar(jmb);
+      
+      JMenu jmFile = new JMenu("File");
+      JMenu jmHelp = new JMenu("Help");
+      
+      //menu items and actionlisteners
+      JMenuItem jmiExit = new JMenuItem("Exit");
+      jmiExit.addActionListener(new ActionListener()
+      {
+         public void actionPerformed(ActionEvent ae)
+         {
+            //informs server we quit
+            JOptionPane.showMessageDialog(null, "Thanks for playing Bompad");
+         }
+      });
+      
+      JMenuItem jmiAbout = new JMenuItem("About");
+      jmiAbout.addActionListener(new ActionListener()
+      {
+         public void actionPerformed(ActionEvent ae)
+         {
+            //details on version, authors
+            //JOptionPane.showMessageDialog(null, );
+         }
+      });
+      
+      JMenuItem jmiInstruct = new JMenuItem("Instructions");
+      jmiInstruct.addActionListener(new ActionListener()
+      {
+         public void actionPerformed(ActionEvent ae)
+         {
+            //set instructions for Bompad
+            String instruct = "";
+            JOptionPane.showMessageDialog(null, instruct);   
+         }
+      });
+   }//end constructor
+}//end class
