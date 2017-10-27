6fbca70d3dd7d0362dd5dcc7f8e12e04dcde173e
Compulex <33073741+Compulex@users.noreply.github.com>
2017-10-26 20:35:49 -0400

    Pond class
    Like my JAVADOCS??

--- Pond.java
+++ Pond.java
@@ -1,6 +1,28 @@
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+import java.util.*;
 
+/**
+   Blabalbalbalbalbalbabbaabllaaaaaaaahhh JAVADOCS
+*/
 
-public class Pond{
-
-
-}
+public class Pond extends JPanel{
+   ArrayList<LilyPad> lilypads = new ArrayList<LilyPad>();
+   
+   public Pond()
+   {
+      this.setLayout(new GridLayout(8,8));
+      for(int i = 0; i < 64; i++)
+      {
+         LilyPad lp = new LilyPad();
+         
+         //random number generator
+         /*if(random == predetermined value)
+         {
+            lp.setBonus(true);
+         }*/
+         lilypads.add(lp);
+      }
+   }//end constructor
+}//end Pond class
