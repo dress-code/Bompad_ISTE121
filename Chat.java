6f62e534cf85294257da034dd56ba4ada2f86db5
Compulex <33073741+Compulex@users.noreply.github.com>
2017-10-26 22:16:35 -0400

    Chat class
    GUI is set :+1:

--- Chat.java
+++ Chat.java
@@ -1,7 +1,31 @@
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
 
-
-
-public class Chat{
-
-
-}
+/**
+   JAVADOCS
+   
+*/
+public class Chat extends JPanel{
+   
+   //private 
+   
+   public Chat()
+   {
+      this.setLayout(new GridLayout(2,1));
+      
+      //displays all the messages
+      JTextArea jtaChat = new JTextArea(10,0);
+      JScrollPane scrollpane = new JScrollPane(jtaChat);
+      
+      JPanel jpSend = new JPanel(new FlowLayout());
+         JTextField jtfMsg = new JTextField(10);
+         jpSend.add(jtfMsg);
+         JButton jbSend = new JButton("Send");
+         jpSend.add(jbSend);
+      
+      this.add(scrollpane);
+      this.add(jpSend);
+      
+   }//end constructor
+}//end Chat class
