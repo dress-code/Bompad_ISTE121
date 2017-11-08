264d4531c5fc091b33cd6b319c541f31cf74665f
Zacharry Georges <zacharryg15@gmail.com>
2017-11-07 19:51:06 -0500

    -

--- Lilypad.java
+++ Lilypad.java
@@ -24,7 +24,6 @@ public class Lilypad extends JButton
    public Lilypad(int _row, int _col){
       row = _row;
       col = _col;
-      this.setPreferredSize(new Dimension(85,85));
       this.setPreferredSize(new Dimension(70,70));
       //Sets the new LilyPad icon to the empty space.
       //jbPad.setIcon(empty);
